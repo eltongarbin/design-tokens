345dfc92d91834c2a35562265ec38bd214516c22
Elton <eltongarbin@gmail.com>
2019-08-03 20:12:49 -0300

    first commit

---
+++ build.js
@@ -0,0 +1,118 @@
+const StyleDictionaryPackage = require('style-dictionary');
+
+// HAVE THE STYLE DICTIONARY CONFIG DYNAMICALLY GENERATED
+
+function getStyleDictionaryConfig(platform) {
+  return {
+    source: ['src/globals/**/*.json', `src/platforms/${platform}/*.json`],
+    platforms: {
+      js: {
+        transforms: [
+          'attribute/cti',
+          'name/cti/constant',
+          'size/rem',
+          'color/hex'
+        ],
+        buildPath: `dist/${platform}/`,
+        prefix: 'token',
+        files: [
+          {
+            destination: 'tokens.js',
+            format: 'javascript/es6'
+          }
+        ]
+      },
+      web: {
+        transforms: [
+          'attribute/cti',
+          'name/cti/constant',
+          'time/seconds',
+          'content/icon',
+          'size/rem',
+          'color/css'
+        ],
+        prefix: 'token',
+        buildPath: 'dist/web/',
+        files: [
+          {
+            destination: 'tokens.scss',
+            format: 'scss/variables'
+          }
+        ]
+      },
+      android: {
+        transforms: [
+          'attribute/cti',
+          'name/cti/constant',
+          'color/hex8android',
+          'size/remToSp',
+          'size/remToDp'
+        ],
+        prefix: 'token',
+        buildPath: 'dist/android/',
+        files: [
+          {
+            destination: 'tokens.colors.xml',
+            format: 'android/colors'
+          },
+          {
+            destination: 'tokens.dimens.xml',
+            format: 'android/dimens'
+          },
+          {
+            destination: 'tokens.font_dimens.xml',
+            format: 'android/fontDimens'
+          },
+          {
+            destination: 'tokens.integers.xml',
+            format: 'android/integers'
+          },
+          {
+            destination: 'tokens.strings.xml',
+            format: 'android/strings'
+          }
+        ]
+      },
+      ios: {
+        transforms: [
+          'attribute/cti',
+          'name/cti/constant',
+          'color/UIColor',
+          'content/objC/literal',
+          'asset/objC/literal',
+          'size/remToPt',
+          'font/objC/literal'
+        ],
+        prefix: 'token',
+        buildPath: 'dist/ios/',
+        files: [
+          {
+            destination: 'tokens.h',
+            format: 'ios/macros'
+          }
+        ]
+      }
+    }
+  };
+}
+
+console.log('Build started...');
+
+// PROCESS THE DESIGN TOKENS FOR THE DIFFEREN PLATFORMS
+
+['web', 'ios', 'android'].map(function(platform) {
+  console.log('\n==============================================');
+  console.log(`\nProcessing: [${platform}]`);
+
+  const StyleDictionary = StyleDictionaryPackage.extend(
+    getStyleDictionaryConfig(platform)
+  );
+
+  StyleDictionary.buildPlatform('js');
+  StyleDictionary.buildPlatform(platform);
+
+  console.log('\nEnd processing');
+});
+
+console.log('\n==============================================');
+console.log('\nBuild completed!');
---
+++ dist/web/tokens.js
@@ -0,0 +1,30 @@
+/**
+ * Do not edit directly
+ * Generated on Sat, 03 Aug 2019 23:01:32 GMT
+ */
+
+export const TOKEN_BUTTON_BORDER_RADIUS = "4px";
+export const TOKEN_BUTTON_TEXT_SIZE = "1rem";
+export const TOKEN_BUTTON_HEIGHT = "40px";
+export const TOKEN_COLOR_BASE_BLACK = "#000000";
+export const TOKEN_COLOR_BASE_GRAY_LIGHT = "#cccccc";
+export const TOKEN_COLOR_BASE_GRAY_MEDIUM = "#999999";
+export const TOKEN_COLOR_BASE_GRAY_DARK = "#111111";
+export const TOKEN_COLOR_BASE_RED = "#ff0000";
+export const TOKEN_COLOR_BASE_GREEN = "#00ff00";
+export const TOKEN_COLOR_PRIMARY = "#0000ff";
+export const TOKEN_COLOR_SECONDARY = "#ff0000";
+export const TOKEN_COLOR_ACTION_PRIMARY = "#0000ff";
+export const TOKEN_COLOR_ACTION_SECONDARY = "#ff0000";
+export const TOKEN_COLOR_ACTION_TERTIARY = "#999999";
+export const TOKEN_COLOR_FONT_BASE = "#000000";
+export const TOKEN_COLOR_FONT_PRIMARY = "#0000ff";
+export const TOKEN_COLOR_FONT_SECONDARY = "#ff0000";
+export const TOKEN_COLOR_FONT_TERTIARY = "#cccccc";
+export const TOKEN_FONT_FAMILY_HEADERS = "Montserrat";
+export const TOKEN_FONT_FAMILY_BASE = "Tahoma, Arial, 'Helvetica Neue', sans";
+export const TOKEN_FONT_PLATFORM_SYSTEM = "Tahoma, Arial, 'Helvetica Neue', sans";
+export const TOKEN_SIZE_FONT_SMALL = "0.75rem"; // the small size of the font
+export const TOKEN_SIZE_FONT_MEDIUM = "1rem"; // the medium size of the font
+export const TOKEN_SIZE_FONT_LARGE = "2rem"; // the large size of the font
+export const TOKEN_SIZE_FONT_BASE = "1rem"; // the base size of the font
