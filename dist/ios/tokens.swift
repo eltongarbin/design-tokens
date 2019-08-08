e1f5f1f1ee6eb4cda541d3e50a53ecc901b73c8f
Elton <eltongarbin@gmail.com>
2019-08-07 22:39:58 -0300

    change tokens values

--- dist/ios/tokens.swift
+++ dist/ios/tokens.swift
@@ -3,36 +3,21 @@
 // tokens.swift
 //
 // Do not edit directly
-// Generated on Tue, 06 Aug 2019 22:28:34 GMT
+// Generated on Thu, 08 Aug 2019 01:18:33 GMT
 //
 
 
 import UIKit
 
 public class DesignTokens {
-    static let TOKEN_BUTTON_BORDER_RADIUS = 4px
-    static let TOKEN_BUTTON_HEIGHT = 50px
-    static let TOKEN_BUTTON_TEXT_SIZE = CGFloat(16.00)
-    static let TOKEN_COLOR_ACTION_PRIMARY = UIColor(red: 0.00, green: 0.00, blue: 1.00, alpha:1)
-    static let TOKEN_COLOR_ACTION_SECONDARY = UIColor(red: 1.00, green: 0.00, blue: 0.00, alpha:1)
-    static let TOKEN_COLOR_ACTION_TERTIARY = UIColor(red: 0.60, green: 0.60, blue: 0.60, alpha:1)
-    static let TOKEN_COLOR_BASE_BLACK = UIColor(red: 0.00, green: 0.00, blue: 0.00, alpha:1)
-    static let TOKEN_COLOR_BASE_GRAY_DARK = UIColor(red: 0.07, green: 0.07, blue: 0.07, alpha:1)
-    static let TOKEN_COLOR_BASE_GRAY_LIGHT = UIColor(red: 0.80, green: 0.80, blue: 0.80, alpha:1)
-    static let TOKEN_COLOR_BASE_GRAY_MEDIUM = UIColor(red: 0.60, green: 0.60, blue: 0.60, alpha:1)
-    static let TOKEN_COLOR_BASE_GREEN = UIColor(red: 0.00, green: 1.00, blue: 0.00, alpha:1)
-    static let TOKEN_COLOR_BASE_RED = UIColor(red: 1.00, green: 0.00, blue: 0.00, alpha:1)
-    static let TOKEN_COLOR_FONT_BASE = UIColor(red: 0.00, green: 0.00, blue: 0.00, alpha:1)
-    static let TOKEN_COLOR_FONT_PRIMARY = UIColor(red: 0.00, green: 0.00, blue: 1.00, alpha:1)
-    static let TOKEN_COLOR_FONT_SECONDARY = UIColor(red: 1.00, green: 0.00, blue: 0.00, alpha:1)
-    static let TOKEN_COLOR_FONT_TERTIARY = UIColor(red: 0.80, green: 0.80, blue: 0.80, alpha:1)
-    static let TOKEN_COLOR_PRIMARY = UIColor(red: 0.00, green: 0.00, blue: 1.00, alpha:1)
-    static let TOKEN_COLOR_SECONDARY = UIColor(red: 1.00, green: 0.00, blue: 0.00, alpha:1)
+    static let TOKEN_COLOR_FONT_PRIMARY = UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha:1)
+    static let TOKEN_COLOR_FONT_SECONDARY = UIColor(red: 0.00, green: 0.00, blue: 0.00, alpha:1)
+    static let TOKEN_COLOR_PRIMARY_DARK = UIColor(red: 0.01, green: 0.06, blue: 0.12, alpha:1)
+    static let TOKEN_COLOR_PRIMARY_DEFAULT = UIColor(red: 0.17, green: 0.21, blue: 0.27, alpha:1)
+    static let TOKEN_COLOR_PRIMARY_LIGHT = UIColor(red: 0.34, green: 0.38, blue: 0.44, alpha:1)
+    static let TOKEN_COLOR_SECONDARY_DARK = UIColor(red: 0.00, green: 0.65, blue: 0.76, alpha:1)
+    static let TOKEN_COLOR_SECONDARY_DEFAULT = UIColor(red: 0.00, green: 0.85, blue: 0.96, alpha:1)
+    static let TOKEN_COLOR_SECONDARY_LIGHT = UIColor(red: 0.42, green: 1.00, blue: 1.00, alpha:1)
     static let TOKEN_FONT_FAMILY_BASE = "San Francisco"
-    static let TOKEN_FONT_FAMILY_HEADERS = "Montserrat"
     static let TOKEN_FONT_PLATFORM_SYSTEM = "San Francisco"
-    static let TOKEN_SIZE_FONT_BASE = CGFloat(16.00)
-    static let TOKEN_SIZE_FONT_LARGE = CGFloat(32.00)
-    static let TOKEN_SIZE_FONT_MEDIUM = CGFloat(16.00)
-    static let TOKEN_SIZE_FONT_SMALL = CGFloat(12.00)
 }
--- dist/web/tokens.js
+++ dist/web/tokens.js
@@ -1,30 +1,15 @@
 /**
  * Do not edit directly
- * Generated on Sat, 03 Aug 2019 23:01:32 GMT
+ * Generated on Thu, 08 Aug 2019 01:18:33 GMT
  */
 
-export const TOKEN_BUTTON_BORDER_RADIUS = "4px";
-export const TOKEN_BUTTON_TEXT_SIZE = "1rem";
-export const TOKEN_BUTTON_HEIGHT = "40px";
-export const TOKEN_COLOR_BASE_BLACK = "#000000";
-export const TOKEN_COLOR_BASE_GRAY_LIGHT = "#cccccc";
-export const TOKEN_COLOR_BASE_GRAY_MEDIUM = "#999999";
-export const TOKEN_COLOR_BASE_GRAY_DARK = "#111111";
-export const TOKEN_COLOR_BASE_RED = "#ff0000";
-export const TOKEN_COLOR_BASE_GREEN = "#00ff00";
-export const TOKEN_COLOR_PRIMARY = "#0000ff";
-export const TOKEN_COLOR_SECONDARY = "#ff0000";
-export const TOKEN_COLOR_ACTION_PRIMARY = "#0000ff";
-export const TOKEN_COLOR_ACTION_SECONDARY = "#ff0000";
-export const TOKEN_COLOR_ACTION_TERTIARY = "#999999";
-export const TOKEN_COLOR_FONT_BASE = "#000000";
-export const TOKEN_COLOR_FONT_PRIMARY = "#0000ff";
-export const TOKEN_COLOR_FONT_SECONDARY = "#ff0000";
-export const TOKEN_COLOR_FONT_TERTIARY = "#cccccc";
-export const TOKEN_FONT_FAMILY_HEADERS = "Montserrat";
-export const TOKEN_FONT_FAMILY_BASE = "Tahoma, Arial, 'Helvetica Neue', sans";
-export const TOKEN_FONT_PLATFORM_SYSTEM = "Tahoma, Arial, 'Helvetica Neue', sans";
-export const TOKEN_SIZE_FONT_SMALL = "0.75rem"; // the small size of the font
-export const TOKEN_SIZE_FONT_MEDIUM = "1rem"; // the medium size of the font
-export const TOKEN_SIZE_FONT_LARGE = "2rem"; // the large size of the font
-export const TOKEN_SIZE_FONT_BASE = "1rem"; // the base size of the font
+export const TOKEN_COLOR_PRIMARY_DEFAULT = "#2c3645";
+export const TOKEN_COLOR_PRIMARY_LIGHT = "#566070";
+export const TOKEN_COLOR_PRIMARY_DARK = "#03101e";
+export const TOKEN_COLOR_SECONDARY_DEFAULT = "#00d8f6";
+export const TOKEN_COLOR_SECONDARY_LIGHT = "#6bffff";
+export const TOKEN_COLOR_SECONDARY_DARK = "#00a6c3";
+export const TOKEN_COLOR_FONT_PRIMARY = "#ffffff";
+export const TOKEN_COLOR_FONT_SECONDARY = "#000000";
+export const TOKEN_FONT_FAMILY_BASE = "myriad-pro, 'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif";
+export const TOKEN_FONT_PLATFORM_SYSTEM = "myriad-pro, 'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif";
